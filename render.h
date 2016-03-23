7567665a762cf3954aeca55aec9c5cb3e67c09d3
Blyd <travis.akablyd@hotmail.com>
2016-03-23 14:39:36 -0400

    render loop updated and stuff

--- render.h
+++ render.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <SDL.h>
 
-extern "C" __declspec(dllexport) inline SDL_Renderer* __cdecl create_renderer(SDL_Window* window, int index, int flags)
+extern "C" __declspec(dllexport) inline SDL_Renderer* __cdecl create_renderer(SDL_Window* window)
 {
-	return SDL_CreateRenderer(window, index, flags);
+	return SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
 extern "C" __declspec(dllexport) inline void __cdecl set_render_draw_color(SDL_Renderer* renderer, int red, int green, int blue, int alpha)
